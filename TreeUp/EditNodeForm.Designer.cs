cfba61ef15fb35375f035d55fefb78fac3a684b5
Artem2021-Step <mistoriwne030405@gmail.com>
2021-03-03 20:23:11 +0200

    first commit

---
+++ TreeUp/EditNodeForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace TreeViewUpdate
+{
+    partial class EditNodeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblMain = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.cbParents = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtName.Location = new System.Drawing.Point(11, 52);
+            this.txtName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.PlaceholderText = "Темка";
+            this.txtName.Size = new System.Drawing.Size(321, 34);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblMain
+            // 
+            this.lblMain.AutoSize = true;
+            this.lblMain.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMain.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblMain.Location = new System.Drawing.Point(88, 10);
+            this.lblMain.Name = "lblMain";
+            this.lblMain.Size = new System.Drawing.Size(147, 21);
+            this.lblMain.TabIndex = 1;
+            this.lblMain.Text = "Фільтрація темки";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSubmit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnSubmit.Location = new System.Drawing.Point(11, 187);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(135, 47);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Зміни теми";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // cbParents
+            // 
+            this.cbParents.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.cbParents.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbParents.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.cbParents.FormattingEnabled = true;
+            this.cbParents.Location = new System.Drawing.Point(11, 110);
+            this.cbParents.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbParents.Name = "cbParents";
+            this.cbParents.Size = new System.Drawing.Size(321, 36);
+            this.cbParents.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(198, 187);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Вихід";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // EditNodeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(342, 256);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbParents);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblMain);
+            this.Controls.Add(this.txtName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "EditNodeForm";
+            this.Text = "Редагування ноди";
+            this.Load += new System.EventHandler(this.EditNodeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblMain;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ComboBox cbParents;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ TreeUp/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace TreeViewUpdate
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvCategories = new System.Windows.Forms.TreeView();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tvCategories
+            // 
+            this.tvCategories.BackColor = System.Drawing.SystemColors.GrayText;
+            this.tvCategories.Location = new System.Drawing.Point(12, 7);
+            this.tvCategories.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tvCategories.Name = "tvCategories";
+            this.tvCategories.Size = new System.Drawing.Size(316, 320);
+            this.tvCategories.TabIndex = 0;
+            this.tvCategories.DoubleClick += new System.EventHandler(this.tvCategories_DoubleClick);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblDescription.Location = new System.Drawing.Point(350, 9);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(338, 21);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "Щоб зауважати тему - натисніть два рази";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSubmit.Location = new System.Drawing.Point(407, 295);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(236, 32);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "ВИЙТИ";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(407, 200);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ЗАУВАЖАТИ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.tvCategories);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainForm";
+            this.Text = "Головна форма";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvCategories;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
